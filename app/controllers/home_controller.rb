d83c8ff5e77201cd49b8caa5ab765089db70e201
Joseph Reis <josephreis@128-110-232-204.uguest.utah.edu>
2012-07-14 16:49:14 -0600

    works...

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
   def index
-    #@content = Post.all
+    @content = Post.all.sample
     #@content = @content.sample
     #@content = Post.find(:all).sample(2)
-    @content = Post.find(:first, :order => "Random()")
+    #@content = Post.find(:first, :order => "Random()")
   end
 end
